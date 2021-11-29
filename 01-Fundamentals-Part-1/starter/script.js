be81a256e6a04c58f9954ec93bfbe96730ed35bc
Juniiie <jrbuking@gmail.com>
2021-11-29 23:53:28 +1300

    Completed Section 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,6 @@
-sum = 12 + 123 + 13;
-console.log(sum);
+var tip15 = 15;
+var tip20 = 20;
+var bill = 275;
+var tip = bill > 50 && bill < 300 ? bill * tip15 / 100 : bill * tip20 / 100;
+
+console.log (tip)
